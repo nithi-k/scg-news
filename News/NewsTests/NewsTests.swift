3d288b13862336f5a020da6959180c02ada508f9
Nithi Kulasiriswatdi <kulasirswatdi@gmail.com>
2023-04-18 01:36:01 +0700

    remove text

--- News/NewsTests/NewsTests.swift
+++ News/NewsTests/NewsTests.swift
@@ -12,7 +12,7 @@ final class NewsTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-s
+
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
